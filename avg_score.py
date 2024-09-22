668bc1bab4eb8781e57359a39ada81e59de8f604
Ramdeni Saikrishna <saikrishna6115@gmail.com>
2024-09-22 12:33:20 +0530

    Delete avg_score.py

--- avg_score.py
+++
@@ -1,8 +0,0 @@
-# Accept a number from the user and check if it is a perfect square
-
-import math
-
-NUM = int(input('Enter a number to check if it is a perfect square'))
-
-ROOT = math.sqrt(NUM)
-
